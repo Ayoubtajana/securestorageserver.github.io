255d13a8da4dfa582258e1ef7d32cea70b3906a4
Ayoub Tajana <132217153+Ayoubtajana@users.noreply.github.com>
2023-05-01 21:19:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 
+<?php
+// The initial directory path (e.g. /www/your_directory/)
+$initial_directory = '';
+// The current directory path
+$current_directory = $initial_directory;
+// Retrieve all files and directories
+$results = glob(str_replace(['[',']',"\f[","\f]"], ["\f[","\f]",'[[]','[]]'], ($current_directory ? $current_directory : $initial_directory)) . '*');
+// If true, directories will appear first in the populated file list
+$directory_first = true; 
+// Sort files
+if ($directory_first) {
+    usort($results, function($a, $b){
+        $a_is_dir = is_dir($a);
+        $b_is_dir = is_dir($b);
+        if ($a_is_dir === $b_is_dir) {
+            return strnatcasecmp($a, $b);
+        } else if ($a_is_dir && !$b_is_dir) {
+            return -1;
+        } else if (!$a_is_dir && $b_is_dir) {
+            return 1;
+        }
+    });
+}
