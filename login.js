924ad3dd564fd216088ba7d8c31f35850fefccd6
Ayoub Tajana <132217153+Ayoubtajana@users.noreply.github.com>
2023-05-01 19:53:22 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,9 @@
-
-var attempt = 3; // Variable to count number of attempts.
+ attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "ayoubtajana" && password == "1-CNA.060"){
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
 return false;
